3ece79cb1d945d8cdf2392e3ed5cb7d4df8165dc
Mehdi Shahidi <mehdi.shahidi@serato.com>
2019-04-23 13:47:28 +1200

    UserGroup message

---
+++ src/Exception/InvalidUserGroupMessageException.php
@@ -0,0 +1,9 @@
+<?php
+namespace Serato\UserProfileSdk\Exception;
+
+use RuntimeException;
+
+class InvalidUserGroupMessageException extends RuntimeException
+{
+    protected $message = "Invalid UserGroup";
+}
