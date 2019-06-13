c44b85a594bc92dd5345173d1ee0f61191d9ad21
Chris Deighton <chris@serato.com>
2019-06-14 11:11:07 +1200

    Adds `Serato\UserProfileSdk\Message\SoftwareDownloadRemove` message type

---
+++ src/Message/SoftwareDownloadRemove.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace Serato\UserProfileSdk\Message;
+
+use Serato\UserProfileSdk\Message\SoftwareDownload;
+
+/**
+ * A message representing the removal of a user's prior downloading a software installer.
+ */
+class SoftwareDownloadRemove extends SoftwareDownload
+{
+}
---
+++ tests/Message/SoftwareDownloadRemoveTest.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace Serato\UserProfileSdk\Test\Message;
+
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+use Serato\UserProfileSdk\Message\SoftwareDownloadRemove;
+
+class SoftwareDownloadRemoveTest extends PHPUnitTestCase
+{
+    public function testSetters()
+    {
+        $userId = 123;
+        $software = 'dj-sub';
+        $os = 'mac';
+        $version = '1.9.10';
+
+        $softwareDownloadRemove = SoftwareDownloadRemove::create($userId)
+                        ->setSoftwareName($software)
+                        ->setOS($os)
+                        ->setVersion($version);
+
+        $this->assertEquals('SoftwareDownloadRemove', $softwareDownloadRemove->getType());
+        $this->assertEquals($software, $softwareDownloadRemove->getSoftwareName());
+        $this->assertEquals($os, $softwareDownloadRemove->getOS());
+        $this->assertEquals($version, $softwareDownloadRemove->getVersion());
+    }
+}
