2581c7fe626b6f4562597b9e8d6cbdb34725ebda
Takayuki Ibaraki <takayuki.ibaraki@serato.com>
2020-03-10 11:54:08 +1300

    Add new message `AuthorizeApplication`
    - This message is to be sent when an application (not a license) has been authorized

---
+++ src/Message/AuthorizeApplication.php
@@ -0,0 +1,88 @@
+<?php
+declare(strict_types=1);
+
+namespace Serato\UserProfileSdk\Message;
+
+use Serato\UserProfileSdk\Message\AbstractMessage;
+
+/**
+ * A message representing an application authorization
+ */
+class AuthorizeApplication extends AbstractMessage
+{
+    const APP_NAME = 'app-name';
+    const AUTHORIZATION_TIME = 'authorization-time';
+
+    /**
+     * Creates a new message instance
+     *
+     * @param int   $userId      User ID
+     * @param array $params      Array of message parameters
+     * @return self
+     */
+    public static function create(int $userId, array $params = []): self
+    {
+        return new static($userId, $params);
+    }
+
+    /**
+     * Gets the name of the app that has been authorized
+     *
+     * @return string|null
+     */
+    public function getAppName(): ?string
+    {
+        return $this->getParam(self::APP_NAME);
+    }
+
+    /**
+     * Sets the name of the app that has been authorized
+     *
+     * @param string $appName
+     * @return self
+     */
+    public function setAppName(string $appName): self
+    {
+        $this->setParam(self::APP_NAME, $appName);
+        return $this;
+    }
+
+    /**
+     * Gets the time that the app has been authorized
+     *
+     * Date format: DATE_ATOM
+     * Example: 2017-08-15T15:52:01+00:00
+     *
+     * @return string|null
+     */
+    public function getAuthorizationTime(): ?string
+    {
+        return $this->getParam(self::AUTHORIZATION_TIME);
+    }
+
+    /**
+     * Sets the time that the app has been authorized
+     *
+     * Date format: DATE_ATOM
+     * Example: 2017-08-15T15:52:01+00:00
+     *
+     * @param string $authorizationTime Time that the authorization was made
+     * @return self
+     */
+    public function setAuthorizationTime(string $authorizationTime): self
+    {
+        $this->setParam(self::AUTHORIZATION_TIME, $authorizationTime);
+        return $this;
+    }
+
+    /**
+     * Sets the authorization time as a UNIX time stamp.
+     *
+     * @param integer $timestamp Timestamp of when the authorization was made
+     * @return self
+     */
+    public function setAuthorizationTimestamp(int $timestamp): self
+    {
+        return $this->setAuthorizationTime(gmdate(DATE_ATOM, $timestamp));
+    }
+}
---
+++ tests/Message/AuthorizeApplicationTest.php
@@ -0,0 +1,77 @@
+<?php
+declare(strict_types=1);
+
+namespace Serato\UserProfileSdk\Test\Message;
+
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+use Serato\UserProfileSdk\Message\AuthorizeApplication;
+
+class AuthorizeApplicationTest extends PHPUnitTestCase
+{
+    /**
+     * Tests that in the default constructed state, the message does not throw exceptions when invoking accessors
+     * to its properties.
+     */
+    public function testDefaultValues(): void
+    {
+        $userId = 123;
+        $message = AuthorizeApplication::create($userId);
+        $this->assertEquals($userId, $message->getUserId());
+        $this->assertEquals([], $message->getParams());
+        $this->assertEquals(null, $message->getAppName());
+        $this->assertEquals(null, $message->getAuthorizationTime());
+    }
+
+    /**
+     * Tests that all setters for the message are working correctly
+     */
+    public function testSetters(): void
+    {
+        $userId = 123;
+        $appName = 'serato_studio';
+        $authorizationTime = '2017-08-15T15:52:01+00:00';
+        $message = AuthorizeApplication::create($userId)
+            ->setAppName($appName)
+            ->setAuthorizationTime($authorizationTime);
+
+        $expectedParams = [
+            AuthorizeApplication::APP_NAME => $appName,
+            AuthorizeApplication::AUTHORIZATION_TIME => $authorizationTime
+        ];
+        
+        $this->assertEquals($userId, $message->getUserId());
+        $this->assertEquals($expectedParams, $message->getParams());
+        $this->assertEquals($appName, $message->getAppName());
+        $this->assertEquals($authorizationTime, $message->getAuthorizationTime());
+        
+        $timestamp = 1583730684;
+        $expectedTime = "2020-03-09T05:11:24+00:00"; # Equivalent to the above
+        $expectedParams = [
+            AuthorizeApplication::APP_NAME => $appName,
+            AuthorizeApplication::AUTHORIZATION_TIME => $expectedTime
+        ];
+        $message->setAuthorizationTimestamp($timestamp);
+        $this->assertEquals($expectedTime, $message->getAuthorizationTime());
+        $this->assertEquals($expectedParams, $message->getParams());
+    }
+
+    /**
+     * Tests that passing in arguments via an array rather than function calls works correctly
+     */
+    public function testSetByParams(): void
+    {
+        $userId = 123;
+        $appName = 'serato_studio';
+        $authorizationTime = '2017-08-15T15:52:01+00:00';
+        $params = [
+            AuthorizeApplication::APP_NAME => $appName,
+            AuthorizeApplication::AUTHORIZATION_TIME => $authorizationTime
+        ];
+        $message = AuthorizeApplication::create($userId, $params);
+
+        $this->assertEquals($userId, $message->getUserId());
+        $this->assertEquals($params, $message->getParams());
+        $this->assertEquals($appName, $message->getAppName());
+        $this->assertEquals($authorizationTime, $message->getAuthorizationTime());
+    }
+}
